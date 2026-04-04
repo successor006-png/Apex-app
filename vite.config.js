6e4281ed0cca9fc71fd23986542604c2d1702c3b
successor006-png <successor006@gmail.com>
2026-04-04 23:43:02 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,20 +19,13 @@ export default defineConfig({
         start_url: '/',
         scope: '/',
         icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
+  {
+    src: 'icon.svg',
+    sizes: 'any',
+    type: 'image/svg+xml',
+    purpose: 'any maskable'
+  }
+],
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
